92e583af99b043b3d5f2f2d80f2edb7cd68837da
Muhammad Azri <muhd.azri@fiction-labs.com>
2018-05-08 07:16:11 +0800

    making active book component a container

--- src/containers/book-details.js
+++ src/containers/book-details.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookDetails extends Component{
+export class BookDetails extends Component{
     render() {
         return(
             <div>Book Details!!!</div>
         );
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        book: state.activeBook
+    };
+}
+
+export default connect(mapStateToProps)(BookDetails);
