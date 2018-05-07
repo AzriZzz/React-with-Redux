236ff5675148c38631e129df8d5a509938a202de
Muhammad Azri <muhd.azri@fiction-labs.com>
2018-05-08 07:32:25 +0800

    print out the title of the currently selected book

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,4 +1,4 @@
-//statte argumemt is not application state, only the state
+//state argument is not application state, only the state
 //this reduce is responsible for
 export default function(state = null,action) {
     switch(action.type) {
